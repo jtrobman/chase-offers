e40babc675ce5cbf6307b943abd229e4f084e54f
Jason Trobman <jason@digitalpulp.com>
2021-11-24 08:37:04 -0500

    empty string was changed back to add to card

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ async function addNextOffer() {
   const WAIT_TIME_MS = 1000
   var offerButtons = document.getElementsByClassName('sixersoffers__cta')
   for (let i = 0; i < offerButtons.length; i++) {
-    // It used to be "add to cart" if not added yet, but now it's just empty string.
-    if (offerButtons[i].getAttribute('aria-label').toLowerCase().includes('')) {
-      console.log('clicking ' + offerButtons[i].getAttribute('aria-label'))
+    if (offerButtons[i].getAttribute('aria-label').toLowerCase().includes('add to card')) {
       offerButtons[i].click()
       setTimeout(() => {
         document.getElementById('flyoutClose').click()
